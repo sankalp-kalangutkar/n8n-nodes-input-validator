12aa052b74af517d9371b6975880406ff9560905
Damian Karzon <dkarzon@outlook.com>
2024-09-16 11:28:48 +1000

    Remove the example files from /dist

--- dist/nodes/CustomTrigger/CustomTrigger.node.d.ts
+++
@@ -1,6 +0,0 @@
-import { INodeType, INodeTypeDescription, ITriggerFunctions, ITriggerResponse, IWebhookFunctions, IWebhookResponseData } from 'n8n-workflow';
-export declare class CustomTrigger implements INodeType {
-    description: INodeTypeDescription;
-    webhook(this: IWebhookFunctions): Promise<IWebhookResponseData>;
-    trigger(this: ITriggerFunctions): Promise<ITriggerResponse>;
-}
